98bf189722eebfe1c67644db58d454e4561e20c3
Bivin Varkey Varghese <bivinvvpacoeg@gmail.com>
2020-03-29 16:43:14 +0530

    fruits page

--- lib/Stores/Fruits.dart
+++ lib/Stores/Fruits.dart
@@ -8,8 +8,179 @@ class Fruits extends StatefulWidget {
 class _FruitsState extends State<Fruits> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                left: 10, top: 100, bottom: 5, right: 10),
+              child: MaterialButton(
+                padding: EdgeInsets.only(left: 5),
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  minWidth: MediaQuery.of(context).size.width * 0.15,
+                  color: Theme.of(context).primaryColor,
+                onPressed: () => Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Fruits())),
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 0),
+                        child: Text(
+                                      "Fruits and Vegetables",
+                                style: TextStyle(fontSize: 15.0),
+                                    ),
+                                    ),
+                            ),
+                    shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(15.0),
+                    ),
+                      elevation: 5,
+      ),
+              )
+              ,Padding(
+                padding: const EdgeInsets.only(
+                    left: 1, top: 100, bottom: 5, right: 20),
+                child: MaterialButton(
+                  padding: EdgeInsets.only(left: 0),
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  minWidth: MediaQuery.of(context).size.width * 0.15,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () => Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Fruits())),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 0),
+                      child: Text(
+                        "Fruits and Vegetables",
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5,
+                ),
+              )
+            ],
+          )
+          ,Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 10, top: 20, bottom: 5, right: 10),
+                child: MaterialButton(
+                  padding: EdgeInsets.only(left: 5),
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  minWidth: MediaQuery.of(context).size.width * 0.15,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () => Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Fruits())),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 0),
+                      child: Text(
+                        "Fruits and Vegetables",
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5,
+                ),
+              )
+              ,Padding(
+                padding: const EdgeInsets.only(
+                    left: 1, top: 20, bottom: 5, right: 20),
+                child: MaterialButton(
+                  padding: EdgeInsets.only(left: 0),
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  minWidth: MediaQuery.of(context).size.width * 0.15,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () => Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Fruits())),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 0),
+                      child: Text(
+                        "Fruits and Vegetables",
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5,
+                ),
+              )
+            ],
+          )
+          ,Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 10, top: 20, bottom: 5, right: 10),
+                child: MaterialButton(
+                  padding: EdgeInsets.only(left: 5),
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  minWidth: MediaQuery.of(context).size.width * 0.15,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () => Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Fruits())),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 0),
+                      child: Text(
+                        "Fruits and Vegetables",
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5,
+                ),
+              )
+              ,Padding(
+                padding: const EdgeInsets.only(
+                    left: 1, top: 20, bottom: 5, right: 20),
+                child: MaterialButton(
+                  padding: EdgeInsets.only(left: 0),
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  minWidth: MediaQuery.of(context).size.width * 0.15,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () => Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Fruits())),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 0),
+                      child: Text(
+                        "Fruits and Vegetables",
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
