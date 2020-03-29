7c6923ce8658bb8c4b828ab8788a2ced733eccf7
Bivin Varkey Varghese <bivinvvpacoeg@gmail.com>
2020-03-29 19:25:54 +0530

    changed fruit.dart

--- lib/Stores/Fruits.dart
+++ lib/Stores/Fruits.dart
@@ -18,22 +18,16 @@ class _FruitsState extends State<Fruits> {
                 padding: const EdgeInsets.only(
                 left: 10, top: 100, bottom: 5, right: 10),
               child: MaterialButton(
-                padding: EdgeInsets.only(left: 5),
+                padding: EdgeInsets.only(left: 0),
                   height: MediaQuery.of(context).size.height * 0.2,
                   minWidth: MediaQuery.of(context).size.width * 0.15,
                   color: Theme.of(context).primaryColor,
                 onPressed: () => Navigator.of(context)
                     .push(MaterialPageRoute(builder: (context) => Fruits())),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 0),
-                        child: Text(
-                                      "Fruits and Vegetables",
-                                style: TextStyle(fontSize: 15.0),
-                                    ),
-                                    ),
-                            ),
+                  child: Ink.image(image: NetworkImage('https://picsum.photos/250?image=9'),
+                  fit: BoxFit.cover,
+                  width: 130,
+                  height: 130,),
                     shape: RoundedRectangleBorder(
                     borderRadius: new BorderRadius.circular(15.0),
                     ),
@@ -50,16 +44,10 @@ class _FruitsState extends State<Fruits> {
                   color: Theme.of(context).primaryColor,
                   onPressed: () => Navigator.of(context)
                       .push(MaterialPageRoute(builder: (context) => Fruits())),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 0),
-                      child: Text(
-                        "Fruits and Vegetables",
-                        style: TextStyle(fontSize: 15.0),
-                      ),
-                    ),
-                  ),
+                  child: Ink.image(image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                    width: 130,
+                    height: 130,),
                   shape: RoundedRectangleBorder(
                     borderRadius: new BorderRadius.circular(15.0),
                   ),
@@ -74,21 +62,21 @@ class _FruitsState extends State<Fruits> {
                 padding: const EdgeInsets.only(
                     left: 10, top: 20, bottom: 5, right: 10),
                 child: MaterialButton(
-                  padding: EdgeInsets.only(left: 5),
+                  padding: EdgeInsets.only(left: 0),
                   height: MediaQuery.of(context).size.height * 0.2,
-                  minWidth: MediaQuery.of(context).size.width * 0.15,
+                  minWidth: MediaQuery.of(context).size.width * 0.1,
+
                   color: Theme.of(context).primaryColor,
-                  onPressed: () => Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) => Fruits())),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 0),
-                      child: Image.asset('path/the_image.png')
-                    ),
-                  ),
                   
-                  shape: RoundedRectangleBorder(
+                  onPressed: () => Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Fruits())),
+
+                  child: Ink.image(image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                    width: 130,
+                    height: 130,)
+
+                  ,shape: RoundedRectangleBorder(
                     borderRadius: new BorderRadius.circular(15.0),
                   ),
                   elevation: 5,
@@ -104,16 +92,10 @@ class _FruitsState extends State<Fruits> {
                   color: Theme.of(context).primaryColor,
                   onPressed: () => Navigator.of(context)
                       .push(MaterialPageRoute(builder: (context) => Fruits())),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 0),
-                      child: Text(
-                        "Fruits and Vegetables",
-                        style: TextStyle(fontSize: 15.0),
-                      ),
-                    ),
-                  ),
+                  child: Ink.image(image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                    width: 130,
+                    height: 130,),
                   shape: RoundedRectangleBorder(
                     borderRadius: new BorderRadius.circular(15.0),
                   ),
@@ -128,22 +110,16 @@ class _FruitsState extends State<Fruits> {
                 padding: const EdgeInsets.only(
                     left: 10, top: 20, bottom: 5, right: 10),
                 child: MaterialButton(
-                  padding: EdgeInsets.only(left: 5),
+                  padding: EdgeInsets.only(left: 0),
                   height: MediaQuery.of(context).size.height * 0.2,
                   minWidth: MediaQuery.of(context).size.width * 0.15,
                   color: Theme.of(context).primaryColor,
                   onPressed: () => Navigator.of(context)
                       .push(MaterialPageRoute(builder: (context) => Fruits())),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 0),
-                      child: Text(
-                        "Fruits and Vegetables",
-                        style: TextStyle(fontSize: 15.0),
-                      ),
-                    ),
-                  ),
+                  child: Ink.image(image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                    width: 130,
+                    height: 130,),
                   shape: RoundedRectangleBorder(
                     borderRadius: new BorderRadius.circular(15.0),
                   ),
@@ -160,16 +136,10 @@ class _FruitsState extends State<Fruits> {
                   color: Theme.of(context).primaryColor,
                   onPressed: () => Navigator.of(context)
                       .push(MaterialPageRoute(builder: (context) => Fruits())),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 0),
-                      child: Text(
-                        "Fruits and Vegetables",
-                        style: TextStyle(fontSize: 15.0),
-                      ),
-                    ),
-                  ),
+                  child: Ink.image(image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                    width: 130,
+                    height: 130,),
                   shape: RoundedRectangleBorder(
                     borderRadius: new BorderRadius.circular(15.0),
                   ),
