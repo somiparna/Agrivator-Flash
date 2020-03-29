6627e88b5f5132697c707337aa761fc68cb31e4c
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2020-03-29 22:11:10 +0530

    Corrected Food Page
    All the images are same.  To be changed.

---
+++ lib/error.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Error extends StatefulWidget {
+  @override
+  _ErrorState createState() => _ErrorState();
+}
+
+class _ErrorState extends State<Error> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+        body: Container(
+      child: Center(
+        child: Text("Error 404! \nPage not Built.",style: TextStyle(fontSize: 27),textAlign: TextAlign.center,),
+      ),
+    ));
+  }
+}
