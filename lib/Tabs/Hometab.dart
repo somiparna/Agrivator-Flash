ecf6b04874a50fc3e75a61d285e8cf9cc980ab9f
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2020-04-06 04:25:02 +0530

    Added Stores Near You
    Deleted all previous UI

--- lib/Tabs/Hometab.dart
+++ lib/Tabs/Hometab.dart
@@ -3,6 +3,7 @@ import 'package:agrivatorflash/Stores/Health.dart';
 import 'package:agrivatorflash/Stores/Meat.dart';
 import 'package:agrivatorflash/Stores/Pet.dart';
 import 'package:agrivatorflash/Stores/Stationary.dart';
+import '../error.dart';
 
 import '../HomeScreen.dart';
 import 'package:flutter/material.dart';
@@ -17,211 +18,199 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   @override
   Widget build(BuildContext context) {
+    var size=MediaQuery.of(context).size;
     return Container(
       color: Colors.white10,
       child: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-//            FlatButton(
-////              minWidth: MediaQuery.of(context).size.width *
-////                  0.8590277777777778,
-//              shape: RoundedRectangleBorder(
-//                  borderRadius: BorderRadius.all(Radius.circular(5))
-//              ),
-//              child: Text(
-//                "Search                                            ",
-//                textAlign: TextAlign.start,
-//                style: TextStyle(
-//                    color: Colors.black,
-//                    decoration: TextDecoration.underline,
-//                    fontSize: 20),
-//              ),
-//              color: Colors.white10,
-//              onPressed: () {
-//              },
-//            ),
             Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, top: 8, bottom: 5, right: 20),
-              child: MaterialButton(
-                padding: EdgeInsets.only(left: 0),
-                height: MediaQuery.of(context).size.height * 0.14,
-                minWidth: MediaQuery.of(context).size.width * 0.85,
-                color: Theme.of(context).primaryColor,
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Fruits())),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 25),
-                    child: Text(
-                      "Fruits and Vegetables",
-                      style: TextStyle(fontSize: 25.0),
-                    ),
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20,),
+                    child: Text("Stores Near You",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
                   ),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(15.0),
-                ),
-                elevation: 5,
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15,),
+                    child: Text("SEE ALL>",style: TextStyle(fontSize: 14)),
+                  )
+                ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, top: 8, bottom: 5, right: 20),
-              child: MaterialButton(
-                padding: EdgeInsets.only(left: 0),
-                height: MediaQuery.of(context).size.height * 0.14,
-                minWidth: MediaQuery.of(context).size.width * 0.85,
-                color: Theme.of(context).primaryColor,
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Medicines())),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 25),
-                    child: Text(
-                      "Medicines",
-                      style: TextStyle(fontSize: 25.0),
+              padding: const EdgeInsets.only(left: 5, top: 15,bottom: 5,),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.only(left: 15,  right: 5),
+                            child: Material(
+                                child: InkWell(
+                                  onTap: () =>
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => Error())),
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+
+                                      child: Image.asset('images/storea.jpg',
+                                          width: 100, height: 100),
+                                    ),),
+                                )
+                            )
+                        ),
+                        Text("Store A",style: TextStyle(fontSize: 13)),
+                        Text("2 KM",style: TextStyle(fontSize: 10,color: Colors.grey),),
+                      ],
                     ),
-                  ),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(15.0),
-                ),
-                elevation: 5,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, top: 8, bottom: 5, right: 20),
-              child: MaterialButton(
-                padding: EdgeInsets.only(left: 0),
-                height: MediaQuery.of(context).size.height * 0.14,
-                minWidth: MediaQuery.of(context).size.width * 0.85,
-                color: Theme.of(context).primaryColor,
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Pets())),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 25),
-                    child: Text(
-                      "Pet Supplies",
-                      style: TextStyle(fontSize: 25.0),
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.only(left: 15,  right: 5),
+                            child: Material(
+                                child: InkWell(
+                                  onTap: () =>
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => Error())),
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+
+                                      child: Image.asset('images/storeb.jpg',
+                                          width: 100, height: 100),
+                                    ),),
+                                )
+                            )
+                        ),
+                        Text("Store B",style: TextStyle(fontSize: 13)),
+                        Text("2.3 KM",style: TextStyle(fontSize: 10,color: Colors.grey),),
+                      ],
                     ),
-                  ),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(15.0),
-                ),
-                elevation: 5,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, top: 8, bottom: 5, right: 20),
-              child: MaterialButton(
-                padding: EdgeInsets.only(left: 0),
-                height: MediaQuery.of(context).size.height * 0.14,
-                minWidth: MediaQuery.of(context).size.width * 0.85,
-                color: Theme.of(context).primaryColor,
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Groceries())),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 25),
-                    child: Text(
-                      "Groceries",
-                      style: TextStyle(fontSize: 25.0),
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.only(left: 15,  right: 5),
+                            child: Material(
+                                child: InkWell(
+                                  onTap: () =>
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => Error())),
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+
+                                      child: Image.asset('images/storec.jpg',
+                                          width: 100, height: 100),
+                                    ),),
+                                )
+                            )
+                        ),
+                        Text("Store C",style: TextStyle(fontSize: 13)),
+                        Text("2.6 KM",style: TextStyle(fontSize: 10,color: Colors.grey),),
+                      ],
                     ),
-                  ),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(15.0),
-                ),
-                elevation: 5,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, top: 8, bottom: 5, right: 20),
-              child: MaterialButton(
-                padding: EdgeInsets.only(left: 0),
-                height: MediaQuery.of(context).size.height * 0.14,
-                minWidth: MediaQuery.of(context).size.width * 0.85,
-                color: Theme.of(context).primaryColor,
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Health())),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 25),
-                    child: Text(
-                      "Health and Benefits",
-                      style: TextStyle(fontSize: 25.0),
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.only(left: 15,  right: 5),
+                            child: Material(
+                                child: InkWell(
+                                  onTap: () =>
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => Error())),
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+
+                                      child: Image.asset('images/stored.jpg',
+                                          width: 100, height: 100),
+                                    ),),
+                                )
+                            )
+                        ),
+                        Text("Store D",style: TextStyle(fontSize: 13)),
+                        Text("2.9 KM",style: TextStyle(fontSize: 10,color: Colors.grey),),
+                      ],
                     ),
-                  ),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(15.0),
-                ),
-                elevation: 5,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, top: 8, bottom: 5, right: 20),
-              child: MaterialButton(
-                padding: EdgeInsets.only(left: 0),
-                height: MediaQuery.of(context).size.height * 0.14,
-                minWidth: MediaQuery.of(context).size.width * 0.85,
-                color: Theme.of(context).primaryColor,
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Meat())),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 25),
-                    child: Text(
-                      "Meat and Fish",
-                      style: TextStyle(fontSize: 25.0),
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.only(left: 15,  right: 5),
+                            child: Material(
+                                child: InkWell(
+                                  onTap: () =>
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => Error())),
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+
+                                      child: Image.asset('images/storee.jpg',
+                                          width: 100, height: 100),
+                                    ),),
+                                )
+                            )
+                        ),
+                        Text("Store E",style: TextStyle(fontSize: 13)),
+                        Text("3.4 KM",style: TextStyle(fontSize: 10,color: Colors.grey),),
+                      ],
                     ),
-                  ),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(15.0),
-                ),
-                elevation: 5,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, top: 8, bottom: 5, right: 20),
-              child: MaterialButton(
-                padding: EdgeInsets.only(left: 0),
-                height: MediaQuery.of(context).size.height * 0.14,
-                minWidth: MediaQuery.of(context).size.width * 0.85,
-                color: Theme.of(context).primaryColor,
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Stationary())),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 25),
-                    child: Text(
-                      "Stationary",
-                      style: TextStyle(fontSize: 25.0),
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.only(left: 15,  right: 5),
+                            child: Material(
+                                child: InkWell(
+                                  onTap: () =>
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => Error())),
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+
+                                      child: Image.asset('images/storef.jpg',
+                                          width: 100, height: 100),
+                                    ),),
+                                )
+                            )
+                        ),
+                        Text("Store F",style: TextStyle(fontSize: 13)),
+                        Text("3.7 KM",style: TextStyle(fontSize: 10,color: Colors.grey),),
+                      ],
                     ),
-                  ),
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.only(left: 15,  right: 5),
+                            child: Material(
+                                child: InkWell(
+                                  onTap: () =>
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => Error())),
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+
+                                      child: Image.asset('images/storea.jpg',
+                                          width: 100, height: 100),
+                                    ),),
+                                )
+                            )
+                        ),
+                        Text("Store G",style: TextStyle(fontSize: 13)),
+                        Text("3.9 KM",style: TextStyle(fontSize: 10,color: Colors.grey),),
+                      ],
+                    ),
+                  ],
                 ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(15.0),
-                ),
-                elevation: 5,
               ),
             ),
           ],
